1a4de06d323d8b994f819defba21800dba5edb1d
Wenhao He <43885049+He-Wenhao@users.noreply.github.com>
2024-06-26 17:48:54 -0400

    Update operate_all.sh

--- orca/operate_all.sh
+++ orca/operate_all.sh
@@ -1,14 +1,34 @@
+#!/bin/bash
+#SBATCH -o log-%j
+#SBATCH -N 20
+#SBATCH --ntasks-per-node 10
+#SBATCH -A DMR23002
+#SBATCH -p development
+#SBATCH -t 2:00:00        # Run time (hh:mm:ss)
 
-#folder_list=$(ls -d */)
-folder_list=$(ls -d */ | grep -v -E '^(C3H4|C4H6)/$')
+# recursively run 
+recurse_run(){
+    if grep -q "all done" log; then
+        echo 'skip'
+        :
+    else
+        rm log
+        bash parallel_pvtz.sh > log 2>&1
+    fi
+}
+
+folder_list=$(ls -d */)
+#folder_list=$(ls -d */ | grep -v -E '^(C3H4|C4H6)/$')
 #for fo in C4H6 C4H8 
 for fo in $folder_list
-do
-cd $fo
-echo $(pwd)
-#cp /scratch1/08491/th1543/script/generate_rad.py .
-#python3 generate_rad.py
-#cp /scratch1/08491/th1543/script/parallel_pvdz.sh .
-bash parallel_pvdz.sh > log 2>&1
-cd ..
+    do
+    cd $fo
+    echo $(pwd)
+    #cp /scratch1/08491/th1543/script/generate_rad.py .
+    #python3 generate_rad.py
+    #cp /scratch1/08491/th1543/script/parallel_pvtz.sh .
+    #rm log
+    recurse_run
+    cd ..
 done
+echo 'done'
